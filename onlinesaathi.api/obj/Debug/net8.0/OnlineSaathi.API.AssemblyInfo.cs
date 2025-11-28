3bbb55b54525de917084503fbec8f293ed3a093e
Hemraj Thapa <108741029+thapahemraj@users.noreply.github.com>
2025-11-28 05:29:49 +0000

    chore: update Angular dependencies to version 20.3.14

--- onlinesaathi.api/obj/Debug/net8.0/OnlineSaathi.API.AssemblyInfo.cs
+++ onlinesaathi.api/obj/Debug/net8.0/OnlineSaathi.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineSaathi.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc761a14f9c7d08301fdbfdf79ebee5448918275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4731752275e6d341c5f8ec309ad3499af5b9cfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineSaathi.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineSaathi.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
